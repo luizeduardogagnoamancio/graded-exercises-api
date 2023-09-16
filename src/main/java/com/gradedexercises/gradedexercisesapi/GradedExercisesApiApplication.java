edf348013d6e47d50283e88597c11fbf45be8e58
Luiz Eduardo Gagno Amancio <68043598+luizeduardogagnoamancio@users.noreply.github.com>
2023-09-15 22:44:26 -0300

    first commit

---
+++ src/main/java/com/gradedexercises/gradedexercisesapi/GradedExercisesApiApplication.java
@@ -0,0 +1,13 @@
+package com.gradedexercises.gradedexercisesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradedExercisesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradedExercisesApiApplication.class, args);
+	}
+
+}
