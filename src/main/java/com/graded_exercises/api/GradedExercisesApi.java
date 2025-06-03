baf5902f75d61f0bf575b362bab8d0d769e80e89
Luiz Eduardo Gagno Amancio <luiz.amancio@spassu.com.br>
2025-06-03 13:14:57 -0300

    First Commit

---
+++ src/main/java/com/graded_exercises/api/GradedExercisesApi.java
@@ -0,0 +1,13 @@
+package com.graded_exercises.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradedExercisesApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradedExercisesApi.class, args);
+	}
+
+}
---
+++ src/test/java/com/graded_exercises/api/GradedExercisesTestAPI.java
@@ -0,0 +1,13 @@
+package com.graded_exercises.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
