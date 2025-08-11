153d576d60879f6ce276ae373adebcb4b3f2b50d
Luiz Eduardo Gagno Amancio <68043598+luizeduardogagnoamancio@users.noreply.github.com>
2025-08-11 20:09:38 -0300

    GRADEX_0003: Registro e login concluido

--- src/main/java/com/graded_exercises/api/dto/request/UserRegisterRequestDTO.java
+++ src/main/java/com/graded_exercises/api/dto/request/UserRegisterRequestDTO.java
@@ -1,6 +1,9 @@
 package com.graded_exercises.api.dto.request;
 
 import com.graded_exercises.api.enums.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,7 +12,14 @@ import lombok.Data;
 @Builder
 @AllArgsConstructor
 public class UserRegisterRequestDTO {
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @NotBlank(message = "Password is required")
     private String password;
+
     private UserRole role;
 }
