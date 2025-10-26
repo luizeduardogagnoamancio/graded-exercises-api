5d2abedd6775490003626294fcb5009e9ba05b7e
luizeduardogagnoamancio <luizgagnoamancio@gmail.com>
2025-10-26 16:29:41 -0300

    GRADEX_0012: Acertos enum

--- src/main/java/com/graded_exercises/api/enums/QuestionType.java
+++ src/main/java/com/graded_exercises/api/enums/QuestionType.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 public enum QuestionType {
     FILL_IN_THE_BLANK("fill_in_the_blank"),
     MULTIPLE_CHOICE("multiple_choice"),
-    DRAG_AND_DROP("drag_and_drop");
+    SENTENCE_SCRAMBLE("sentence_scramble");
 
     private final String value;
 
