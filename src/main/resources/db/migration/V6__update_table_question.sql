a7666778530d78edca8c4ca34ec7e3910c538a18
Luiz Eduardo Gagno Amancio <luizgagnoamancio@gmail.com>
2025-10-27 17:16:38 -0300

    GRADEX_0012: Acerto para remoção de exercicios tipo DRAG_AND_DROP

--- src/main/resources/db/migration/V6__update_table_question.sql
+++ src/main/resources/db/migration/V6__update_table_question.sql
@@ -1,31 +1,31 @@
 UPDATE GRD_EX_QUESTION
 SET QUESTION_CONTENT = JSONB_SET(
     QUESTION_CONTENT::jsonb,
-    '{formats,DRAG_AND_DROP}',
-    '{"sentenceParts": ["I", "___", " tired."], "options": ["is", "am", "are"], "correctAnswer": "am"}'::jsonb
+    '{formats,SENTENCE_SCRAMBLE}',
+    '{"correctOrder": ["I", "am", "tired."], "shuffledOptions": ["tired.", "I", "am"]}'::jsonb
 )
 WHERE QUESTION_ID = 1;
 
 UPDATE GRD_EX_QUESTION
 SET QUESTION_CONTENT = JSONB_SET(
     QUESTION_CONTENT::jsonb,
-    '{formats,DRAG_AND_DROP}',
-    '{"sentenceParts": ["You", "___", " nice."], "options": ["is", "am", "are"], "correctAnswer": "are"}'::jsonb
+    '{formats,SENTENCE_SCRAMBLE}',
+    '{"correctOrder": ["You", "are", "nice."], "shuffledOptions": ["nice.", "You", "are"]}'::jsonb
 )
 WHERE QUESTION_ID = 2;
 
 UPDATE GRD_EX_QUESTION
 SET QUESTION_CONTENT = JSONB_SET(
     QUESTION_CONTENT::jsonb,
-    '{formats,DRAG_AND_DROP}',
-    '{"sentenceParts": ["He", "___", " cool."], "options": ["is", "am", "are"], "correctAnswer": "is"}'::jsonb
+    '{formats,SENTENCE_SCRAMBLE}',
+    '{"correctOrder": ["He", "is", "cool."], "shuffledOptions": ["cool.", "is", "He"]}'::jsonb
 )
 WHERE QUESTION_ID = 3;
 
 UPDATE GRD_EX_QUESTION
 SET QUESTION_CONTENT = JSONB_SET(
     QUESTION_CONTENT::jsonb,
-    '{formats,DRAG_AND_DROP}',
-    '{"sentenceParts": ["She", "___", " pretty."], "options": ["is", "am", "are"], "correctAnswer": "is"}'::jsonb
+    '{formats,SENTENCE_SCRAMBLE}',
+    '{"correctOrder": ["She", "is", "pretty."], "shuffledOptions": ["is", "pretty.", "She"]}'::jsonb
 )
 WHERE QUESTION_ID = 4;
